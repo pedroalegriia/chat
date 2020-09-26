7045c0ed21f0ec0821192746b55fb17170f027bd
alexis castaneda segura <acastaneda@savas.com.mx>
2020-09-25 22:24:20 -0500

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Ozelot Technologies on 25/09/20.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
