08b6647bee87b7fbfb13851f4de85624a03a873c
Pedro Alegria <pedro_alegriia@savas.com.mx>
2020-09-25 23:24:13 -0500

    90 mins

---
+++ Chat/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Chat
+//
+//  Created by Ozelot Technologies on 25/09/20.
+//
+
+import Firebase
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
---
+++ Chat/ChatHostViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ChatHostViewController.swift
+//  Chat
+//
+//  Created by Ozelot Technologies on 25/09/20.
+//
+
+import Foundation
